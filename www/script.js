ba6cdd1953614e11f4bea6f085f6bb789a30edef
Markus Daghall <markus@daghall.se>
2024-03-22 16:19:32 +0100

    Basic training

--- www/script.js
+++ www/script.js
@@ -14,7 +14,7 @@
 
 const main = document.getElementsByTagName("main")[0];
 const vowels = [ "a", "i", "u", "e", "o" ];
-const consonants = [ "", "k", "s", "t", "n", "h", "m", "y", "r", "w" ];
+const consonants = [ "", "k", "s", "t", "n", "h", "m", "y", "r", "w", "N" ];
 const hiragana = {
   "": {		a: "あ", i: "い", u: "う", e: "え", o: "お" },
   "k": { 	a: "か", i: "き", u: "く", e: "け", o: "こ" },
@@ -29,8 +29,78 @@ const hiragana = {
   "N": "ん",
 };
 
+function train(kanas) {
+  const randomized = shuffleKanas(kanas);
+  console.log({randomized}); // eslint-disable-line no-console
+  let disableSubmit = false;
 
-function draw(kanas) {
+  const form = document.createElement("form");
+  const largeKana = document.createElement("span");
+  const message = document.createElement("span");
+  const answer = document.createElement("input");
+
+  const correct = {};
+  const wrong = {};
+
+  form.className = "training";
+  largeKana.className = "large-kana";
+  message.className = "message";
+  answer.type = "text";
+  answer.className = "answer";
+  form.onsubmit = () => {
+    if (disableSubmit) return false;
+    disableSubmit = true;
+
+    try {
+      const key = substituteSound(answer.name, true);
+      const name = answer.name.toLowerCase();
+      if (answer.value.toLowerCase() === name) {
+        correct[key] = true;
+        message.textContent = `${name}`;
+        message.classList.add("message--correct");
+        console.log("correct"); // eslint-disable-line no-console
+      } else {
+        message.textContent = `${name}`;
+        message.classList.add("message--wrong");
+        wrong[key] = true;
+        console.log(`WRONG – "${answer.value}" should have been "${answer.name}"`); // eslint-disable-line no-console
+      }
+
+      if (randomized.length > 0) {
+        setTimeout(() => {
+          drawFoo(randomized.pop());
+          disableSubmit = false;
+        }, 1000);
+      } else {
+        drawKana(kanas, correct, wrong);
+      }
+    } catch (e) {
+      console.log({e}); // eslint-disable-line no-console
+    }
+
+    return false;
+  };
+
+  form.appendChild(largeKana);
+  form.appendChild(message);
+  form.appendChild(answer);
+
+  drawMain(form);
+  drawFoo(randomized.pop());
+
+  function drawFoo({ name, kana }) {
+    largeKana.textContent = kana;
+    message.textContent = "";
+    message.classList.remove("message--correct", "message--wrong");
+    answer.name = name;
+    answer.value = "";
+    answer.focus();
+  }
+}
+
+
+function drawKana(kanas, correct = {}, wrong = {}) {
+  console.log({correct, wrong}); // eslint-disable-line no-console
   const table = document.createElement("table");
   const header = document.createElement("tr");
   header.appendChild(document.createElement("th"));
@@ -47,19 +117,36 @@ function draw(kanas) {
     const row = document.createElement("tr");
     const rowHeader = document.createElement("th");
 
-    rowHeader.innerText = consonant;
+    rowHeader.innerText = consonant.toLowerCase();
     row.appendChild(rowHeader);
 
-    vowels.forEach((vowel) => {
-      const kana = kanas[consonant][vowel];
+    if (typeof kanas[consonant] === "object") {
+      vowels.forEach((vowel) => {
+        const kana = kanas[consonant][vowel];
+        const cell = document.createElement("td");
+
+        if (kana) {
+          cell.innerText = kana;
+          if (correct[consonant + vowel]) {
+            cell.className = "correct";
+          } else if (wrong[consonant + vowel]) {
+            cell.className = "wrong";
+          }
+        }
+
+        row.appendChild(cell);
+      });
+    } else {
       const cell = document.createElement("td");
+      cell.textContent = kanas[consonant];
 
-      if (kana) {
-        cell.innerText = kana;
+      if (correct[consonant]) {
+        cell.className = "correct";
+      } else if (wrong[consonant]) {
+        cell.className = "wrong";
       }
-
       row.appendChild(cell);
-    });
+    }
 
     table.appendChild(row);
   });
@@ -75,4 +162,53 @@ function drawMain(element) {
   main.appendChild(element);
 }
 
-draw(hiragana);
+
+function shuffleKanas(kanas) {
+  const result = [];
+  const flatKanas = [];
+  const rows = Object.keys(kanas);
+
+  rows.forEach((row) => {
+    const current = kanas[row];
+
+    if (typeof current === "object") {
+      const cols = Object.entries(current);
+      cols.forEach(([vowel, kana]) => {
+        flatKanas.push({
+          name: substituteSound(row + vowel),
+          kana,
+        });
+      });
+    } else {
+      flatKanas.push({
+        name: row,
+        kana: current,
+      });
+    }
+  });
+
+  while (flatKanas.length > 0) {
+    const randomIndex = Math.floor(Math.random() * flatKanas.length);
+    const [item] = flatKanas.splice(randomIndex, 1);
+    result.push(item);
+  }
+
+  return result;
+}
+
+function substituteSound(kana, reverse = false) {
+  const substitituions = {
+    "si": "shi",
+    "ti": "chi",
+    "tu": "tsu",
+    "hu": "fu",
+  };
+
+  if (reverse) {
+    return Object.entries(substitituions).find(([, val]) => val === kana)?.[0] || kana;
+  } else {
+    return substitituions[kana] || kana;
+  }
+}
+
+train(hiragana);
